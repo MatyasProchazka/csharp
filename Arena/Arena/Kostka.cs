0b98d6b8b8bced14ab217128b0a0da35c9693419
Matyas <matyasekprochazka@seznam.cz>
2023-02-10 01:52:04 +0100

    vytvoreni noveho oop projektu areny

---
+++ Arena/Arena/Kostka.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arena
+{
+    internal class Kostka
+    {
+        /// <summary>
+        /// pocet stran kostky
+        /// </summary>
+        private int pocetStran;
+        /// <summary>
+        /// generator nahodny cisel
+        /// </summary>
+        private Random random;
+        ///
+        public Kostka(int aPocetStran = 6)
+        {
+            pocetStran = aPocetStran;
+            random = new Random();
+        }
+        /// <summary>
+        /// vrátí počet stran hraci kostky
+        /// </summary>
+        /// <returns>pocet stran hraci kostky</returns>
+        public int VratPocetStran()
+        {
+            return pocetStran;
+        }
+        /// <summary>
+        /// vykona hod kostkou
+        /// </summary>
+        /// <returns>cislo od 1 do poctu stran</returns>
+        public int Hod()
+        {
+            return random.Next(1, pocetStran + 1);
+        }
+        public override string ToString()
+        {
+            return String.Format("kostka s {0} stranami", pocetStran);
+        }
+    }
+}
---
+++ Arena/Arena/Program.cs
@@ -0,0 +1,6 @@
+﻿using Arena;
+
+Kostka kostka = new Kostka();
+Console.WriteLine(kostka.VratPocetStran());
+Console.WriteLine(kostka);
+Console.ReadKey(); 
