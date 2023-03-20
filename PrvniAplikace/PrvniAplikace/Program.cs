7136bfb872532c179e6c3927852e0325264565d4
Matyas <matyasekprochazka@seznam.cz>
2023-03-20 01:40:10 +0100

    uprava slozek

--- PrvniAplikace/PrvniAplikace/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using PrvniAplikace;
-
-Zdravic zdravic = new Zdravic();
-zdravic.text = "programatore";
-Console.WriteLine(zdravic.Pozdrav("Matyas"));
-Console.WriteLine(zdravic.Pozdrav("Honza"));
-Console.ReadKey();
